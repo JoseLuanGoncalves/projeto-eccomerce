7b08775d8eaefeec6d7304a263ffbac44e65cf0b
JoseLuanGoncalves <luangoncalves190416@gmail.com>
2025-08-14 15:00:27 -0300

    adiciona a funcionalidade de salvar os produtos presentes no carrinho

---
+++ js/carrinho.js
@@ -0,0 +1,49 @@
+const btnAddCarrinho = document.querySelectorAll(".aicionar-carrinho");
+
+btnAddCarrinho.forEach((btn) => {
+  btn.addEventListener("click", (evento) => {
+    const elementoProduto = evento.target.closest(".produto");
+    const produtoID = elementoProduto.dataset.id;
+    const produtoNome = elementoProduto.querySelector(".nome").textContent;
+    const produtoPreco = parseFloat(
+      elementoProduto
+        .querySelector(".preco")
+        .textContent.replace("R$ ", "")
+        .replace(".", "")
+        .replace(",", ".")
+    );
+    const produtoImagem = elementoProduto
+      .querySelector(".imagem")
+      .getAttribute("src");
+
+    const carrinho = obterProdutosDoCarrinho();
+
+    const produtoExistente = carrinho.find(
+      (produto) => produto.id === produtoID
+    );
+
+    if (produtoExistente) {
+      produtoExistente.quantidade += 1;
+    } else {
+      carrinho.push({
+        id: produtoID,
+        nome: produtoNome,
+        preco: produtoPreco,
+        imagem: produtoImagem,
+        quantidade: 1,
+      });
+    }
+
+    salvarProdutosNoCarrinho(carrinho);
+
+  });
+});
+
+function obterProdutosDoCarrinho() {
+  const produtos = localStorage.getItem("carrinho");
+  return produtos ? JSON.parse(produtos) : [];
+}
+
+function salvarProdutosNoCarrinho(carrinho) {
+        localStorage.setItem("carrinho", JSON.stringify(carrinho));
+}
